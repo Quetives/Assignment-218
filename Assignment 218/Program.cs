8d3c7f806ae9a3ca6e12dd934eeb4a661d327085
Richard <40587587+Quetives@users.noreply.github.com>
2020-03-12 17:38:19 -0700

    Add project files.

---
+++ Assignment 218/Program.cs
@@ -0,0 +1,22 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.IO;
+
+namespace Assignment_218
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Please enter a number:");
+            string number = string.Format(Console.ReadLine());
+            File.WriteAllText(@"C:\Users\Richard\Documents\logs\number.txt", number);
+            string text = File.ReadAllText(@"C:\Users\Richard\Documents\logs\number.txt");
+            Console.WriteLine(text);
+            Console.ReadLine();
+        }
+    }
+}
